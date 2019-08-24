1e0dd285052a216200997ec423a1c411d5e15195
岩永彩里 <catherinrin@gmail.com>
2019-08-24 13:30:14 +0900

    Section2

--- Landmarks/Landmarks/Profile/ProfileHost.swift
+++ Landmarks/Landmarks/Profile/ProfileHost.swift
@@ -9,10 +9,22 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var mode
     @State var profile = Profile.default
+    @State var drafiProfile = Profile.default
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: self.profile)
+            HStack {
+                Spacer()
+                
+                EditButton()
+            }
+            if self.mode?.wrappedValue == .inactive {
+                ProfileSummary(profile: profile)
+            } else {
+                Text("Profile Editor")
+            }
+            
         }
         .padding()
     }
